f203ca732ef79e81bf73ba38666ebadd71748630
petushokok <olekhova.ekaterina@gmail.com>
2025-04-10 12:21:59 +0300

    добавила главную страницу и кнопку вернуться на главный экран на бинаризации

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from app.api.auth import router as auth_router
 from app.api.lab import router as lab_router
 from app.db.db import Base, engine
+from pathlib import Path
 
 Base.metadata.create_all(bind=engine)  # Создаём таблицы в SQLite
+BASE_DIR = Path(__file__).resolve().parent.parent
+templates = Jinja2Templates(directory=str(BASE_DIR / "2lab/app/templates"))
 
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(lab_router)
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Auth API"}
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request})
